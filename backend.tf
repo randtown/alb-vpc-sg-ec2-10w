fcadb1326229b7097a26328a3c0db23b43e79c4e
Randy <randy.townsend45@gmail.com>
2026-04-25 13:38:22 -0500

    remove the real bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "albterraformstate-rt"
+    bucket  = "replace here with your bucket name"
     key     = "alb/terraform.state"
     region  = "us-east-1"
     encrypt = true
